1243e24edec8b0145c905512c77394fd218ca699
Patrick Bashizi <bashizip@gmail.com>
2025-11-28 09:05:18 +0100

    fix: remove duplicate AbadaEngine constructor signature

--- src/main/java/com/abada/engine/core/AbadaEngine.java
+++ src/main/java/com/abada/engine/core/AbadaEngine.java
@@ -57,10 +57,6 @@ public class AbadaEngine {
     public AbadaEngine(PersistenceService persistenceService, TaskManager taskManager, @Lazy EventManager eventManager,
             @Lazy JobScheduler jobScheduler, ExternalTaskRepository externalTaskRepository, ObjectMapper om,
             EngineMetrics engineMetrics, Tracer tracer) {
-
-    public AbadaEngine(PersistenceService persistenceService, TaskManager taskManager, @Lazy EventManager eventManager,
-            @Lazy JobScheduler jobScheduler, ExternalTaskRepository externalTaskRepository, ObjectMapper om,
-            EngineMetrics engineMetrics, Tracer tracer) {
         this.persistenceService = persistenceService;
         this.parser = new BpmnParser();
         this.taskManager = taskManager;
