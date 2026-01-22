3a5ca60b73a886d85c6093fcfbf1f4e14af38476
Patrick Bashizi <bashizip@gmail.com>
2026-01-22 14:38:37 +0100

    cleanup unused imports

--- src/main/java/com/abada/engine/persistence/repository/ExternalTaskRepository.java
+++ src/main/java/com/abada/engine/persistence/repository/ExternalTaskRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,11 +14,12 @@ public interface ExternalTaskRepository extends JpaRepository<ExternalTaskEntity
      * Finds an available external task for a given topic that is either OPEN
      * or has an expired lock.
      *
-     * @param topicName The topic to search for.
-     * @param status The OPEN status.
+     * @param topicName  The topic to search for.
+     * @param status     The OPEN status.
      * @param topicName2 The topic to search for (repeated for the OR clause).
-     * @param now The current time, to check for expired locks.
+     * @param now        The current time, to check for expired locks.
      * @return An Optional containing an available task, if one exists.
      */
-    Optional<ExternalTaskEntity> findFirstByTopicNameAndStatusOrTopicNameAndLockExpirationTimeLessThan(String topicName, ExternalTaskEntity.Status status, String topicName2, Instant now);
+    Optional<ExternalTaskEntity> findFirstByTopicNameAndStatusOrTopicNameAndLockExpirationTimeLessThan(String topicName,
+            ExternalTaskEntity.Status status, String topicName2, Instant now);
 }
